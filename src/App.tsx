134e77aa699680f243d3d95765fb08cffba1cf21
Charles Allamby <cpallamby@gmail.com>
2024-03-13 11:50:09 +1100

    fixed some issues with image displaying. started work on carousel

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,49 @@ import NavBar from "./components/navbar";
 import CardDemo from "./components/productCard";
 import Test from "./components/test";
 import Hero from "./components/hero";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./components/carousel";
+import goeyDoughnut from "./img/goeyDoughnut.jpg";
 
 function App() {
   return (
-    <div>
+    <div className="h-auto w-auto">
       <NavBar />
       <Hero />
-      <div className="m-h-full -z-10 min-w-full bg-[url('./img/doughnut.jpg')] pt-10"></div>
+      {/* <div className="m-h-full -z-10 min-w-full bg-[url('./img/doughnut.jpg')] pt-10"></div> */}
 
+      <div>dada</div>
+
+      <Carousel
+        className=""
+        opts={{
+          loop: true,
+          slidesToScroll: 3,
+          dragFree: true,
+        }}
+      >
+        <CarouselContent className="justify-center">
+          <CarouselItem className="md:basis-1/4 lg:basis-1/4">
+            <img src={require("./img/wildDoughnut.jpg")} />
+          </CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/4">
+            <img src={require("./img/fancyDoughnut.jpg")}></img>
+          </CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/4">
+            <img src={require("./img/goeyDoughnut.jpg")} />
+          </CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/4">
+            <img src={require("./img/goeyDoughnut.jpg")} />
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
       {/* <p className="z-10">hello</p> */}
     </div>
   );
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 
-const pink = "#f1bfda";
+// import doughnut2 from "../img/doughnut2.jpg";
+// import doughnut from "../img/doughnut.jpg";
 
 const Hero = () => {
   return (
-    <div className="bg-cover pt-10" style={{ backgroundColor: pink }}>
-      hero
-    </div>
+    <>
+      {/* <img
+        src="../img/doughnut2.jpg"
+        alt="Your Image"
+        className="h-auto max-w-full"
+      /> */}
+
+      {/* <div className="z-0 h-screen w-screen max-w-full bg-[url('./img/doughnut2.jpg')] bg-contain bg-no-repeat object-contain "> */}
+      <div className=" inline-block">
+        <img src={require("../img/doughnut2.jpg")}></img>
+        <div className=" relative -top-[83rem] left-[13rem] font-serif text-8xl leading-snug  ">
+          Charlies Bakes
+        </div>
+      </div>
+      {/* </div> */}
+    </>
   );
 };
 
